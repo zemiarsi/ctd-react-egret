1771271bb2cf2bfdf4560ddcd1ce0d7eeba4bd6d
zemiarsi <engbetelehem@yahoo.com>
2021-12-14 23:34:41 -0500

    React Final Project

--- lesson_1.1/src/components/TodoListItem.js
+++ lesson_1.1/src/components/TodoListItem.js
@@ -1,22 +1,27 @@
 import React from "react";
-import style from './TodoListItem.module.css'
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import PropTypes from 'prop-types'
-
+import style from "./TodoListItem.module.css";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import PropTypes from "prop-types";
 
 function TodoListItem({ todo, onRemoveTodo }) {
   console.log({ todo });
   return (
     <div>
-      <li className={style.ListItem} >
-        <input type='checkbox' fontSize='medium' className={style.CheckBox}/> 
-      <span className={ todo.fields.completed===true? style.CompletedListItem:''}>
-      {todo.fields.Title}
-      </span>
-        <button 
-        className={style.RmvButton} 
-        type="button" onClick={() => onRemoveTodo(todo.id)} >
-          <RemoveCircleIcon  color='primary' fontSize='medium'/>
+      <li className={style.ListItem}>
+        <input type="checkbox" fontSize="medium" className={style.CheckBox} />
+        <span
+          className={
+            todo.fields.completed === true ? style.CompletedListItem : ""
+          }
+        >
+          {todo.fields.Title}
+        </span>
+        <button
+          className={style.RmvButton}
+          type="button"
+          onClick={() => onRemoveTodo(todo.id)}
+        >
+          <RemoveCircleIcon color="primary" fontSize="medium" />
         </button>
       </li>
     </div>
@@ -24,6 +29,6 @@ function TodoListItem({ todo, onRemoveTodo }) {
 }
 TodoListItem.prototype = {
   todo: PropTypes.object,
-  onRemoveTodo: PropTypes.func
-}
+  onRemoveTodo: PropTypes.func,
+};
 export default TodoListItem;
